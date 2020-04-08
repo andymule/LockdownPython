d9b9e13ccd2db8606d7460de1b046eb0a3091f8d
Andy Muehlhausen <andymule@users.noreply.github.com>
2020-04-08 10:59:38 +0200

    Create ch1-2.test.py

---
+++ ch1-2.test.py
@@ -0,0 +1,47 @@
+##### PART 1
+
+print('\t\tPART 1')
+
+print('Whats yer name, bucko?:')
+# TODO ask user for input, store it in a string
+
+print('How many times has your mother called you?:')
+# TODO ask user for input, store it in an int
+
+print('What did she sound like when she was calling?:')
+# TODO print your name as many times as your mother called you
+
+print('Unbelievable. I heard her do that ten times! ... it sounded like:')
+# TODO print your name as many times as your mother called you times ten
+
+
+##### PART 2
+print('\n\t\tPART 2:')
+
+w1 = " people"
+w2 = "Modify "
+w3 = " me!"
+w8 = True
+w9 = False
+w10 = 5
+w11 = 1.5
+w12 = "It's "
+w13 = " that "
+w14 = " have "
+w15 = " hands"
+w16 = " fingers."
+w17 = " and "
+w18 = " head!"
+
+people_count = 5
+hands_count = 2
+fingers_count = 10
+
+# TODO Using only the variables above and modifying just the single print statement below print this sentence:
+# It's True that 5 people have 10 hands and 100 fingers.
+# You may only use string concatenation, string casting, and multiplication.
+print(w2 + w3)
+
+# BONUS
+# Using ONLY type casting and the variables above (no math, no declaration, etc), print another line that says:
+#  and 1 head!
